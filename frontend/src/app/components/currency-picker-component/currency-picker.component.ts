6c8a561c4815c6ff7ff6897b2c6835dc4c2b3d0b
Manuel <manuel.kirschner@st.othr.de>
2018-11-21 19:48:41 +0100

    Changed logic and added additional value-button components.

--- frontend/src/app/components/currency-picker-component/currency-picker.component.ts
+++ frontend/src/app/components/currency-picker-component/currency-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, Injectable, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-currency-picker',
@@ -7,11 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrencyPickerComponent implements OnInit {
 
-  constructor() { }
+  private _amount:string = '';
+  commaInput: boolean = false;
 
-  addEntry(input : number){
+  constructor() {}
+
+  addEntry(input : string){
     console.log('clicked ' + input);
+
+    if (input.includes(",")) {
+      this.commaInput = true;
+    }
+
+    if (input.startsWith("C")) {
+      this._amount = input.substr(input.indexOf("C") + 1);
+      this.commaInput = false;
+    } else {
+      this._amount = this._amount + input;
+    }
   }
+
+  submit() {
+    console.log('submit value ' + this._amount);
+  }
+
+  get amount() { return this._amount; }
+
 
   ngOnInit() {
   }
--- frontend/src/app/components/value-button/value-button.component.ts
+++ frontend/src/app/components/value-button/value-button.component.ts
@@ -8,6 +8,8 @@ import {Component, Input, OnInit} from '@angular/core';
 export class ValueButtonComponent implements OnInit {
 
   private _value: number = 0;
+  private _text: string = '';
+  private _isActivated: boolean = true;
 
   constructor() {
   }
@@ -15,10 +17,33 @@ export class ValueButtonComponent implements OnInit {
   ngOnInit() {
   }
 
-  get value() { return this._value; }
+  get value() {
+    return this._value;
+  }
+
+  get text() {
+    if (this._text === '') {
+      return this._value.toString();
+    } else {
+      return this._text;
+    }
+  }
+
 
   @Input()
   set value(val: number) {
     this._value = val;
+  }
+
+  @Input()
+  set text(txt: string) {
+      this._text = txt;
+  }
+
+  get isActivated() { return this._isActivated; }
+
+  @Input()
+  set isActivated(activated: boolean) {
+    this._isActivated = activated;
   }
 }
