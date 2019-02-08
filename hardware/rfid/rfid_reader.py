f6e327cc37bbbf0808bfe23da78a75323f72f10a
Manuel <manuel.kirschner@st.othr.de>
2019-02-08 15:27:54 +0100

    added rfid reader components

--- hardware/rfid/rfid_reader.py
+++ hardware/rfid/rfid_reader.py
@@ -1,17 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#
+#    Copyright 2014,2018 Mario Gomez <mario.gomez@teubi.co>
+#
+#    This file is part of MFRC522-Python
+#    MFRC522-Python is a simple Python implementation for
+#    the MFRC522 NFC Card Reader for the Raspberry Pi.
+#
+#    MFRC522-Python is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    MFRC522-Python is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with MFRC522-Python.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
 import json
 
+continue_reading = True
+
+# Capture SIGINT for cleanup when the script is aborted
+def end_read(signal, frame):
+    global continue_reading
+    print("Ctrl+C captured, ending read.")
+    continue_reading = False
+    GPIO.cleanup()
+
+
 async def read_rfid():
-    return 1, "Data"
+    # Hook the SIGINT
+    signal.signal(signal.SIGINT, end_read)
+
+    # Create an object of the class MFRC522
+    MIFAREReader = MFRC522.MFRC522()
+
+    # This loop keeps checking for chips. If one is near it will get the UID and authenticate
+    while continue_reading:
+        # Scan for cards
+        (status, tag_type) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+        # If a card is found
+        if status == MIFAREReader.MI_OK:
+            print("Card detected")
+
+        # Get the UID of the card
+        (status, uid) = MIFAREReader.MFRC522_Anticoll()
+
+        # If we have the UID, continue
+        if status == MIFAREReader.MI_OK:
+
+            # Print UID
+            print("Card read UID: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3]))
+
+    return tag_type, uid
 
 
 # asynchronous send of barcode scan
 async def rfid_producer(websocket, path):
     # start video capturing
     # cam = VideoCamera('/dev/video0') # pi config
-    
+    # Welcome message
+    print("Welcome to the MFRC522 data read example")
+    print("Press Ctrl-C to stop.")
+
     print("[INFO] starting rfid reading ...")
-    rfidType, rfidData = await read_rfid()
-    await websocket.send(json.dumps({'type': rfidType, 'value': rfidData}))
-    
+    rfid_type, rfid_data = await read_rfid()
+    await websocket.send(json.dumps({"type": rfid_type, "value": rfid_data}))
+
     # do a bit of cleanup
-    print("[INFO] stopping and cleaning up of video stream mess ...")
+    print("[INFO] stopping rfid reading and cleaning up ...")
