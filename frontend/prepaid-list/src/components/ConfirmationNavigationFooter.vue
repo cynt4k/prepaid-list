93b3fed88984b786c0e3807cd607d00b8544be34
Rafael Muselmann <rafael.muselmann@oth-regensburg.de>
2019-03-29 16:31:41 +0100

    Snackbar duration and updated ConfirmationFooter.

--- frontend/prepaid-list/src/components/ConfirmationNavigationFooter.vue
+++ frontend/prepaid-list/src/components/ConfirmationNavigationFooter.vue
@@ -1,10 +1,9 @@
 <template>
   <v-footer>
-    <slot/>
-    <v-btn color="success" class="next-btn">
-        Bestätigen
-        <v-icon right>mdi-check-circle-outline</v-icon>
-      </v-btn>
+    <v-btn color="success" class="next-btn" @click="acceptOrder()">
+      Bestätigen
+      <v-icon right>mdi-check-circle-outline</v-icon>
+    </v-btn>
   </v-footer>
 </template>
 
@@ -13,8 +12,10 @@ import { Component, Vue, Prop } from 'vue-property-decorator';
 
 @Component({})
 export default class ConfirmationNavigationFooter extends Vue {
-  @Prop({ default: null })
-    private buttonTitleNext!: string;
+    private acceptOrder(): void {
+        // ToDo to backend: send order
+        // Reset shopping cart state.
+    }
 }
 </script>
 
@@ -28,30 +29,5 @@ footer {
     width: 100%;
     height: 50px !important;
 }
-.cart-chip > span {
-    font-size: 16px;
-    font-weight: bold;
-    cursor: pointer !important;
-}
 
-.scaleInAndOut {
-    animation-name: scaleInAndOut;
-    z-index: 8;
-}
-@keyframes scaleInAndOut {
-    0% {
-        transform: scale(1);
-    }
-    50% {
-        transform: scale(1.5);
-    }
-    100% {
-        transform: scale(1);
-    }
-}
-.animated {
-    animation-duration: 0.8s;
-    animation-iteration-count: 1;
-    animation-direction: alternate;
-}
 </style>
