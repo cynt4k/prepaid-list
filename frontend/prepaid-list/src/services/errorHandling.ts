f9b710c0e659c1db11ac1a9807caa9cd587f30b7
Rafael Muselmann <rafael.muselmann@gmx.de>
2019-10-20 10:23:24 +0200

    Adds new error handling: - user not allowed

--- frontend/prepaid-list/src/services/errorHandling.ts
+++ frontend/prepaid-list/src/services/errorHandling.ts
@@ -10,6 +10,8 @@ export class ErrorHandlingService implements IErrorHandlingService {
         switch (errorCode) {
             case 'E_LOW_BALANCE':
                 return { type: ErrorType.WARN, message: 'Nicht genügend Guthaben'};
+            case 'W_USER_NOT_ALLOWED':
+                return {type: ErrorType.WARN, message: 'Fehlende Berechtigung'};
             default:
                 return { type: ErrorType.ERROR, message: 'Es ist ein Fehler aufgetreten.' };
         }
